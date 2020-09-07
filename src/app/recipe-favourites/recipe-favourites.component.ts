392b045acb831087636e2230b5dc8908e51b56c8
Jus Lozej <jlozej@gmail.com>
2020-09-07 13:03:40 +0200

    Favorites page

--- src/app/recipe-favourites/recipe-favourites.component.ts
+++ src/app/recipe-favourites/recipe-favourites.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import { RecipeService } from "../recipe.service";
 @Component({
   selector: 'app-recipe-favourites',
   templateUrl: './recipe-favourites.component.html',
   styleUrls: ['./recipe-favourites.component.scss']
 })
 export class RecipeFavouritesComponent implements OnInit {
+  favoriteRecipes: object[];
 
-  constructor() { }
+  removeRecipe(index) {
+    this.favoriteRecipes.splice(index, 1);
+  }
+
+  fetchFavorites() {
+    this.recipeService.getFavorites().subscribe((data) => {
+      this.favoriteRecipes = data;
+    });
+  }
+
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+    this.fetchFavorites()
   }
 
 }
