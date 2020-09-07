98829ea2b758b1baf6026490bf4034e99d091ff1
Jus Lozej <jlozej@gmail.com>
2020-09-07 12:59:24 +0200

    App navigation menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RecipeSearchComponent } from './recipe-search/recipe-search.component';
 import { RecipeComponent } from './recipe/recipe.component';
 import { RecipeFavouritesComponent } from './recipe-favourites/recipe-favourites.component';
 import { ApiKeyInterceptor } from './api-key.interceptor';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +21,16 @@ import { ApiKeyInterceptor } from './api-key.interceptor';
     RecipeSearchComponent,
     RecipeComponent,
     RecipeFavouritesComponent
+    NavigationMenuComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
