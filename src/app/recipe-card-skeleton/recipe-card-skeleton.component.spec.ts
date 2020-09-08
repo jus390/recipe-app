170302a56b483e4ccb0e9ad691a1c98b266628f4
Jus <jlozej@gmail.com>
2020-09-08 13:46:18 +0200

    Added card loading skeletons

---
+++ src/app/recipe-card-skeleton/recipe-card-skeleton.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RecipeCardSkeletonComponent } from './recipe-card-skeleton.component';
+
+describe('RecipeCardSkeletonComponent', () => {
+  let component: RecipeCardSkeletonComponent;
+  let fixture: ComponentFixture<RecipeCardSkeletonComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RecipeCardSkeletonComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RecipeCardSkeletonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/recipe-card-skeleton/recipe-card-skeleton.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-card-skeleton',
+  templateUrl: './recipe-card-skeleton.component.html',
+  styleUrls: ['./recipe-card-skeleton.component.scss']
+})
+export class RecipeCardSkeletonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
